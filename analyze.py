045d3727ffb18ac5f4c707b3d638f52e7759e58e
Deymos01 <bananofruslan@gmail.com>
2024-05-19 16:37:00 +0300

    updated

--- analyze.py
+++ analyze.py
@@ -1,58 +1,34 @@
+import numpy as np
 import pandas as pd
 
-mono = pd.read_excel('БД_с_моно_full.xlsx')
-no_mono = pd.read_excel('БД_без_моно_full.xlsx')
-d_stat = pd.read_excel('Показатель_D 1.xlsx')
-f_stat = pd.read_excel('Показатель_F 1.xlsx')
+bDWithoutMono = pd.read_excel(r'БД_без_моно_full.xlsx')
+bDMono = pd.read_excel(r'БД_с_моно_full.xlsx')
 
+# add column about mono
+bDWithoutMono['isMono'] = False
+bDMono['isMono'] = True
+# rename
+bDMono.rename(columns={"Vacin": "Vac"}, inplace=True)
+# Concatenate the DataFrames by rows
+concatenated_data = pd.concat([bDWithoutMono, bDMono])
+# Найдем строки с отсутствующими значениями в столбце 'CaseID'
+missing_caseid_mask = concatenated_data['CaseID'].isna()
+# Присвоим уникальные идентификаторы для отсутствующих значений
+concatenated_data.loc[missing_caseid_mask, 'CaseID'] = range(390078, missing_caseid_mask.sum() + 390078)
+# Сортировка по 'CaseID' и 'Date' так, чтобы самые свежие даты были первыми
+concatenated_data = concatenated_data.sort_values(by='Start')
+# Удаление дубликатов, оставляя только запись с самой свежей датой для каждого 'CaseID'
+unique_data = concatenated_data.drop_duplicates(subset='CaseID', keep='first')
+# save
+unique_data.to_excel(r'Объединенные_данные.xlsx', index=False)
+mergedData = pd.read_excel('Объединенные_данные.xlsx')
+d1 = pd.read_excel(r'Показатель_D 1.xlsx')
+f1 = pd.read_excel(r'Показатель_F 1.xlsx')
+d_stat = d1.drop_duplicates(subset="CaseID")
 
-# print(pd.merge(mono, no_mono, on='CaseID', how='inner'))
-mono.rename(columns={"Vacin": "Vac"}, inplace=True)
-mono['is_mono'] = True
-no_mono['is_mono'] = False
-union_mono = pd.concat([mono, no_mono]) # соединение по строкам
-
-
-union_mono = pd.read_excel('Объединенные_данные.xlsx')
-# print(sum((union_mono.duplicated(subset='CaseID'))))
-union_mono = (union_mono.sort_values(["Start"])).drop_duplicates(subset="CaseID")
-# print(sum((union_mono.duplicated(subset='CaseID'))))
-
-# print(sum((d_stat.duplicated(subset='CaseID'))))
-d_stat = d_stat.drop_duplicates(subset="CaseID")
-# print(sum((d_stat.duplicated(subset='CaseID'))))
-
-# print(sum((f_stat.duplicated(subset='CaseID'))))
-f_stat = (f_stat.sort_values(["Дата взятия"])).drop_duplicates(subset="CaseID")
-# print(sum((f_stat.duplicated(subset='CaseID'))))
+f_stat = (f1.sort_values(["Дата взятия"])).drop_duplicates(subset="CaseID")
 
 f_stat.rename(columns={"Результат": "Результат_F"}, inplace=True)
 union_results_df = pd.merge(d_stat[["CaseID", "Результат_D"]], f_stat[["CaseID", "Результат_F"]], on="CaseID", how="inner")
-
-
-# print(mono.info())
-# print(union_mono.columns)
-# print(union_results_df.columns)
-
-union_result = pd.merge(union_mono, union_results_df, on="CaseID", how="inner")
-union_result.to_excel("result_table.xlsx")
-
-# print(sum((union_result.duplicated(subset='CaseID'))))
-
-# print(union_result.info())
-
-# isalive = pd.DataFrame(union_result.columns)
-
-(union_result.loc[union_result["Outcome"] == "Умер"]).to_excel("isalive.xlsx")
-
-union_results_df.to_excel("DF con.xlsx")
-
-
-
-
-# print(mono.info())
-#
-# print(no_mono.info())
-# print(d_stat.info())
-# print(f_stat.info())
-
+union_results = pd.merge(mergedData, union_results_df, on="CaseID", how="inner")
+union_results.to_excel(r'финальные_данные.xlsx', index=False)
