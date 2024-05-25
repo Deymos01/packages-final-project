9e495d0ed187f15c78079652702c5fb6d7e18424
HikkMind <liferisinghigh@yandex.ru>
2024-05-26 00:01:02 +0300

    update 4th task

--- task4d.py
+++ task4d.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from scipy import stats
 
-groupA = pd.read_excel('.\\groups\\group_A.xlsx')
-groupB = pd.read_excel('.\\groups\\group_B.xlsx')
+groupA = pd.read_excel('.\\groups\\isMono_false.xlsx')
+groupB = pd.read_excel('.\\groups\\isMono_true.xlsx')
 
 t_stat, p_stat = stats.ttest_ind(groupA['Age'], groupB['Age'], equal_var=True)
 print('Age : t = ', t_stat, ' p = ', p_stat)
-t_stat, p_stat = stats.ttest_ind(groupA['Result_D'], groupB['Result_D'], equal_var=True)
+t_stat, p_stat = stats.ttest_ind(groupA['Результат_D'], groupB['Результат_D'], equal_var=True)
 print('Result_D : t = ', t_stat, ' p = ', p_stat)
-t_stat, p_stat = stats.ttest_ind(groupA['Result_F'], groupB['Result_F'], equal_var=True, nan_policy='omit')
+t_stat, p_stat = stats.ttest_ind(groupA['Результат_F'], groupB['Результат_F'], equal_var=True, nan_policy='omit')
 print('Result_F : t = ', t_stat, ' p = ', p_stat)
 
 print(groupA['Age'].describe())
