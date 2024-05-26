306b5bb6738d6801bfc22cffc468389a06d242de
HikkMind <liferisinghigh@yandex.ru>
2024-05-26 12:08:57 +0300

    add vac in 6th

--- task6_d.py
+++ task6_d.py
@@ -33,33 +33,36 @@ plt.show()
 
 
 
+# Сравнение по факту вакцинации
+data_vac = pd.read_excel('data2\\people_who_was_vaccinated.xlsx')
+data_novac = pd.read_excel('data2\\people_who_was`t_vaccinated.xlsx')
+data_vac = data_vac[data_vac['Outcome'].isin(['Выписан', 'Умер'])]
+data_vac['Outcome'] = data_vac['Outcome'] == 'Выписан'
+data_novac = data_novac[data_novac['Outcome'].isin(['Выписан', 'Умер'])]
+data_novac['Outcome'] = data_novac['Outcome'] == 'Выписан'
 
-
-isMono['Vacin'] = (isMono['Vacin'] != 'Нет')
-age_counts = isMono.groupby('Outcome')['Vacin'].mean()*100
+mono_factor = data_vac.groupby('isMono')['Outcome'].mean() * 100
 plt.figure(figsize=(10, 6))
-age_counts.plot(kind='bar', color=['blue', 'green'])
-plt.xlabel('Drug Usage')
+mono_factor.plot(kind='bar', color=['blue', 'green'])
+plt.xlabel('is mono')
 plt.ylabel('Survival Rate (%)')
-plt.title('Survival Rate by Drug Usage')
+plt.title('Impact mono for vaccinated')
 plt.xticks(rotation=0)  # Поворот меток по оси x для удобства чтения
+plt.yticks(range(0, 101, 10))
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
-# plt.plot(age_counts.index, age_counts.values, marker='o', label='with mono')
-# plt.xlabel('Outcome')
-# plt.ylabel('Vacine rate')
-# plt.title('Age distribution according to outcome')
-# plt.xticks(range(min(isMono['Age']), max(isMono['Age']) + 1, 10))
-# plt.grid(axis='y', linestyle='--', alpha=0.7)
-# plt.show()
+mono_factor = data_novac.groupby('isMono')['Outcome'].mean() * 100
+plt.figure(figsize=(10, 6))
+mono_factor.plot(kind='bar', color=['blue', 'green'])
+plt.xlabel('is mono')
+plt.ylabel('Survival Rate (%)')
+plt.title('Impact mono for not vaccinated')
+plt.xticks(rotation=0)  # Поворот меток по оси x для удобства чтения
+plt.yticks(range(0, 101, 10))
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
 
-noMono['Outcome'] = (noMono['Outcome'] == 'Выписан')
-# age_counts = noMono['Age'].value_counts(normalize=True).sort_index()*100
-age_counts = noMono.groupby('Age')['Outcome'].mean()*100
-plt.plot(age_counts.index, age_counts.values, marker='o', label='with no mono')
-# plt.xlabel('Age')
-# plt.ylabel('Percentage')
-plt.xticks(range(min(noMono['Age']), max(noMono['Age']) + 1, 10))
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.legend()
-plt.show()
+
+
+
+
